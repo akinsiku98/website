a859da2f2ce38ffe0f68b7624861f1adba4e7b94
akinsiku98 <166594109+akinsiku98@users.noreply.github.com>
2025-07-27 03:21:25 -0500

    Full portfolio layout with animations and interactivity

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Show "Back to Top" button on scroll
 $(window).scroll(function () {
   if ($(this).scrollTop() > 200) {
     $('#back-to-top').fadeIn();
@@ -6,7 +5,14 @@ $(window).scroll(function () {
     $('#back-to-top').fadeOut();
   }
 });
-// Fade sections
+
+// Smooth scroll to top
+$('#back-to-top').click(function () {
+  $('html, body').animate({ scrollTop: 0 }, 600);
+  return false;
+});
+
+// Fade in sections on scroll
 $(window).on('scroll', function () {
   $('.fade-section').each(function () {
     const topOfElement = $(this).offset().top;
@@ -32,23 +38,4 @@ $(window).click(function (e) {
   if ($(e.target).hasClass('modal')) {
     $(e.target).fadeOut();
   }
-});
-
-
-// Smooth scroll to top
-$('#back-to-top').click(function () {
-  $('html, body').animate({ scrollTop: 0 }, 600);
-  return false;
-});
-
-// Fade in sections on scroll
-$(window).on('scroll', function () {
-  $('.fade-section').each(function () {
-    const topOfElement = $(this).offset().top;
-    const bottomOfWindow = $(window).scrollTop() + $(window).height();
-
-    if (bottomOfWindow > topOfElement + 100) {
-      $(this).addClass('visible');
-    }
-  });
 });
