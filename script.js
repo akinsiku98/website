e7a05db5c36daa7d58ac96f42b3669e586b48dc5
akinsiku98 <166594109+akinsiku98@users.noreply.github.com>
2025-07-27 03:05:08 -0500

    Full portfolio layout with animations and interactivity

--- script.js
+++ script.js
@@ -6,6 +6,34 @@ $(window).scroll(function () {
     $('#back-to-top').fadeOut();
   }
 });
+// Fade sections
+$(window).on('scroll', function () {
+  $('.fade-section').each(function () {
+    const topOfElement = $(this).offset().top;
+    const bottomOfWindow = $(window).scrollTop() + $(window).height();
+
+    if (bottomOfWindow > topOfElement + 100) {
+      $(this).addClass('visible');
+    }
+  });
+});
+
+// Show/Hide Modals
+$('.view-more').click(function () {
+  const target = $(this).closest('.project-card').data('target');
+  $('#' + target).fadeIn();
+});
+
+$('.close').click(function () {
+  $(this).closest('.modal').fadeOut();
+});
+
+$(window).click(function (e) {
+  if ($(e.target).hasClass('modal')) {
+    $(e.target).fadeOut();
+  }
+});
+
 
 // Smooth scroll to top
 $('#back-to-top').click(function () {
